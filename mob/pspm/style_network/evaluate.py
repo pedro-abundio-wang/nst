02a9b48603c8adb5ccec75cc39bfa193f8df00a3
pedro-abundio-wang <pedro.abundio.wang@gmail.com>
2021-08-10 08:35:42 +0800

    style network

--- mob/pspm/style_network/evaluate.py
+++ mob/pspm/style_network/evaluate.py
@@ -1,5 +1,6 @@
 from utils import tensor_to_image, load_img, clip, resolve_video
 from mob.pspm.style_network.models import transformation_network
+from mob.pspm.style_network.models import feed_forward
 
 image_type = ('jpg', 'jpeg', 'png', 'bmp')
 
@@ -8,7 +9,7 @@ def transfer(content, weights, max_dim, result):
 
     if content[-3:] in image_type:
         # Build the feed-forward network and load the weights.
-        transformation_model = transformation_network()
+        transformation_model = feed_forward()
         transformation_model.load_weights(weights).expect_partial()
 
         # Load content image.
